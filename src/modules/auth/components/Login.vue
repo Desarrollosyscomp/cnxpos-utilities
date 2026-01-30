99e04b5ed49ecaf6a6e321d315557487e7a32600
Rommel Montoya <rommelmontoya97@gmail.com>
2026-01-30 14:48:08 -0500

    login updated

--- src/modules/auth/components/Login.vue
+++ src/modules/auth/components/Login.vue
@@ -1,13 +1,19 @@
 <template>
-    <div>
-
-        loginnnn
+    <div class="login-container">
+        <input class="form-input" type="text" placeholder="Usuario">
+        <input class="form-input" type="password" placeholder="Contraseña">
     </div>
 </template>
 <script setup>
 
 
 </script>
-<style scoped>
 
+<style scoped>
+@import '../../../styles/forms.css';
+.login-container{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
 </style>
