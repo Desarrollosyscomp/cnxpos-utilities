f44a2b0035c411b47b4a7ffe571e0e24a102ca3c
Fernanda Astaiza <yulieth.astaiza@soy.sena.edu.co>
2026-02-02 11:30:08 -0500

    implementation mockup login

--- src/modules/auth/components/desktop/Login.vue
+++ src/modules/auth/components/desktop/Login.vue
@@ -1,19 +1,58 @@
 <template>
     <div class="login-container">
         <input class="form-input" type="text" placeholder="Usuario">
-        <input class="form-input" type="password" placeholder="Contraseña">
+        <input class="form-input" :type="showPassword ? 'text' : 'password'" placeholder="Contraseña">
+        <!-- <svg-icon type="mdi" :path="path"></svg-icon> -->
+        <a href="#" class="text-color">Olvidé mi contraseña</a>
     </div>
+    <div class="button-container">
+        <button class="form-button">Iniciar Sesión</button>
+    </div>
+    
+
 </template>
 <script setup>
+import { ref } from 'vue-demi';
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAccount } from '@mdi/js'
 
+let showPassword = ref(false);
 
 </script>
 
 <style scoped>
 @import '../../../../styles/forms.css';
 .login-container{
+    width: 80%;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    gap: 20px;
+}
+
+.form-input{
+    background-color: transparent;
+    font-size: 16px;
+    border-radius: 10px;
+    border: 1px solid var(--color-contrast);
+    color: var(--color-contrast);
+}
+.text-color{
+    color: var(--color-contrast);
+    text-decoration: none;
+    font-size: 16px;
+}
+.button-container{
+    width: 50%;
+}
+.form-button{
+    background-color: var(--color-contrast);
+    color: var(--color-primary);
+    border-radius: 10px;
+    border: none;
+    margin-top: 20px;
+    padding: 10px;
+    cursor: pointer;
+    width: 100%;
+    font-size: 16px;
 }
 </style>
--- src/utils/theme-transitions.ts
+++ src/utils/theme-transitions.ts
@@ -1,7 +1,12 @@
+import { ref } from "vue"
+
+export let visible = ref(false)
+
 export const toggleTheme = () => {
     const html = document.documentElement
     html.classList.add('theme-transition')
     const theme = html.getAttribute('data-theme')
     html.setAttribute('data-theme',theme === 'dark' ? 'light' : 'dark')
     setTimeout(()=>{html.classList.remove('theme-transition')},500)
+    visible.value = !visible.value
 }
