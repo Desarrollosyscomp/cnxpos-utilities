db5e33c4499f8ddbdddf96507b1f07208b9fe4bd
Rommel Montoya <rommelmontoya97@gmail.com>
2026-02-20 11:24:26 -0500

    routers fixed

--- src/apps/web-reporter/modules/dashboard/router/dashboard.routes.ts
+++ src/apps/web-reporter/modules/dashboard/router/dashboard.routes.ts
@@ -1,16 +1,15 @@
-const getComponent = () => {
-    if (import.meta.env.VITE_TARGET === 'mobile') {
-        // @ts-ignore
-        return import('../../../../../responsive-switcher/MobileHomeSwitcher.vue')
-    } else {
-        // @ts-ignore
-        return import('../../../../../responsive-switcher/HomeSwitcher.vue')
-    }
-}
 export const DashboardRoutes = [
     {
         path: '/home',
-        component: getComponent(),
+        component: () => {
+            if (import.meta.env.VITE_TARGET === 'mobile') {
+                // @ts-ignore
+                return import('../../../../../responsive-switcher/MobileHomeSwitcher.vue')
+            } else {
+                // @ts-ignore
+                return import('../../../../../responsive-switcher/HomeSwitcher.vue')
+            }
+        },
 
         children: [
             {
