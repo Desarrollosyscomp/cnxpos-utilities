92c321f3537d4dd81be4af4b3b52d0b70a1931bd
Fernanda Astaiza <yulieth.astaiza@soy.sena.edu.co>
2026-02-23 15:09:00 -0500

    minor bugs fixed

--- src/modules/home/components/desktop/DesktopHome.vue
+++ src/modules/home/components/desktop/DesktopHome.vue
@@ -17,7 +17,7 @@
           <span class="item-label font-montserrat-medium">Web Reporter</span>
         </div>
         <div class="item">
-          <Icon :path="mdiCurrencyUsd" class="icon" />
+          <Icon :path="mdiCurrencyUsd" class="icon" @click="router.push('/web-report-v2/daily-sales')"/>
           <span class="item-label font-montserrat-medium">Resumen de ventas</span>
         </div>
       </div>
--- src/modules/home/components/mobile/MobileHome.vue
+++ src/modules/home/components/mobile/MobileHome.vue
@@ -16,7 +16,7 @@
           <span>Web Reporter</span>
         </div>
         <div class="item">
-          <Icon :path="mdiCurrencyUsd" :class="'icon'" />
+          <Icon :path="mdiCurrencyUsd" :class="'icon'" @click="router.push('/web-report-v2/daily-sales')"/>
           <span>Resumen de ventas</span>
         </div>
       </div>
